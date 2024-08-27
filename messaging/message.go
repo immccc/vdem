f4ae9d9e0bf413231b5160dd0a4d465a11e14481
Ismael <efc24ff0-b463-4b3e-a6b4-2f6c3c62b7b1@anonaddy.me>
2024-08-27 18:01:22 +0200

    Messaging between nodes

--- messaging/message.go
+++ messaging/message.go
@@ -1,7 +1,24 @@
 package messaging
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 const (
 	EventMsgType = "EVENT"
 	ReqMsgType   = "REQ"
 	CloseType    = "CLOSE"
 )
+
+type MessageType string
+
+func BuildMessageWithEvent(event *Event) string {
+	eventAsJson, err := json.Marshal(event)
+	if err != nil {
+		log.Printf("Unable to build Message, can't convert to JSON: %v !", err)
+	}
+
+	return fmt.Sprintf("[%v, %v]", EventMsgType, eventAsJson)
+}
